8985c8c3cb038113221bf7a0a6fa8b85816ebb58
Susa1ev <95218345+Susa1ev@users.noreply.github.com>
2021-11-29 15:28:00 +0300

    Add files via upload

---
+++ Задание №1.2(косольное приложение).cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace prac1._2_console_
+{
+    class Prac
+    {
+        private int a, b;
+        private string chislo;
+        private char[] bukva;
+        private void inner()
+        {
+            Console.Write("Введите трёхзначное число:");
+            chislo = Console.ReadLine();
+        }
+        private void sravnenie()
+        {
+            if (Int32.TryParse(chislo, out a) && chislo.Length == 3)
+            {
+                bukva = chislo.ToCharArray();
+                a = Int32.Parse(bukva[0].ToString());
+                b = Int32.Parse(bukva[1].ToString());
+                if (a > b)
+                {
+                    Console.WriteLine("Первая цифра(" + a + ") больше второй(" + b + ")");
+                }
+                else if (a < b)
+                {
+                    Console.WriteLine("Вторая цифра(" + b + ") больше первой(" + a + ")");
+                }
+                else if (a == b)
+                {
+                    Console.WriteLine("Первая цифра(" + a + ") равна второй(" + b + ")");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не трёхзначное число");
+            }
+        }
+        public Prac()
+        {
+            inner();
+            sravnenie();            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prac prac = new Prac();
+        }
+    }
+}
---
+++ Задание№1.1(консольное приложение).cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace prac1_console_
+{
+    class Circle
+    {
+        private double Radius = 0.0;
+        private double Area = 0.0;
+        private double Pi = Math.PI;
+        public Circle()
+        {
+            fillArea();
+            showCircle();
+        }
+
+        public void fillArea()
+        {
+            Console.Write("Введите площадь окружности: ");
+            Double.TryParse(Console.ReadLine(), out Area);
+        }
+        public void showCircle()
+        {
+            Radius = Math.Sqrt(Area / Pi);
+            Console.Write("R = (" + Area + "/Пи)^1/2 = " + Radius);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            Console.ReadKey();
+        }
+    }
+}
